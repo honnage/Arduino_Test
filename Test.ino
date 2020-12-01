0503b2fff142502141bd989462e4eeb564cffff9
honnage <hon_da-1998@hotmail.com>
2020-12-02 00:08:56 +0700

    Plotte View Wave Form with Serial Plotte.

--- Test.ino
+++ Test.ino
@@ -1,29 +1,28 @@
 const int ledPin1 = D7;
 const int ledPin2 = D6;
 
-int timeOn = 400;
-int timeOff = 400;
+int timeOn = 1000;
+int timeOff = 1000;
 
 void setup() {
-// pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+
   pinMode(ledPin1,OUTPUT);
   pinMode(ledPin2,OUTPUT);
 
   Serial.begin(9600);
-  Serial.print("LED On/Off ");
-  Serial.println(timeOn + timeOff);
+  //Serial.print("LED On/Off ");
+  //Serial.println(timeOn + timeOff);
 }
 
-// the loop function runs over and over again forever
 void loop() {
   
   digitalWrite(ledPin1, HIGH);  
   digitalWrite(ledPin2, HIGH);  
-  Serial.println("LED On");
+  Serial.println(1);
   delay(timeOn); 
   
   digitalWrite(ledPin1, LOW);   
   digitalWrite(ledPin2, LOW);  
-  Serial.println("LED Off");
+  Serial.println(0);
   delay(timeOff);  
 }
