549994a289497b8aac3114779f0be6e4c35e2927
honnage <hon_da-1998@hotmail.com>
2020-12-03 00:23:22 +0700

    Adjustable frequency blinker

--- AnalogSensor/AnalogSensor.ino
+++ AnalogSensor/AnalogSensor.ino
@@ -1,14 +1,25 @@
 const int analogInPin = A0;
 int sensorValue = 0;
 
+const int ledPin = D7;
+int adcValue = 0;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   sensorValue = analogRead(analogInPin);
-  Serial.println(sensorValue);
-  delay(40);
+  //adcValue = sensorValue;
+  adcValue = map(sensorValue, 4, 1024,100, 1500);
+  
+  Serial.println(adcValue);
+  
+  digitalWrite(ledPin, HIGH);  
+  delay(adcValue);
+  
+  digitalWrite(ledPin, LOW);  
+  delay(adcValue);
   
 }
