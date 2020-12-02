dc619d961ff092b0a88005e817d44cd23bb35997
honnage <hon_da-1998@hotmail.com>
2020-12-03 00:47:35 +0700

    Control the LED brightness Serial Monitor

--- AnalogSensor/AnalogSensor.ino
+++ AnalogSensor/AnalogSensor.ino
@@ -2,22 +2,24 @@
 //int sensorValue = 0;
 
 const int ledPin = D7;
-int adcValue = 0;
+//int adcValue = 0;
 
-int delayTime = 50;
+//int delayTime = 50;
 int duty;
 
 void setup() {
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
+  Serial.println("Enter 0 - 1024");
 }
 
 void loop() {
 
-  for (duty = 0; duty < 1024; duty += 10){
+  if( Serial.available() > 0){
+    duty =  Serial.parseInt();
+
     analogWrite(ledPin, duty);
     Serial.println(duty);
-    delay(delayTime);
   }
-  
+  delay(10);
 }
